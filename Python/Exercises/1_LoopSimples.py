902786d6ced417d206df882d22a25e38318ea3e4
beagle27866 <bamaral2.git2023@gmail.com>
2024-10-10 10:18:49 -0400

    update: 1_LoopSimples.py analysis are done

--- Python/Exercises/1_LoopSimples.py
+++ Python/Exercises/1_LoopSimples.py
@@ -1,28 +1,28 @@
 def loopSimples(n):
-    for i in range(n):
-        print(i)
+    for i in range(n): # 1 operation
+        print(i) # 1 operation
 
 """
 Analysis
 
 #1 Set variables
-
+- n: the amount of times that the loop must iterate
 -------------------------------------------------------------------------------
 
 #2 Count operations
-
+- 2 operations (increment i and print i)
 -------------------------------------------------------------------------------
 
 #3 Mathematical expression
-
+- T(n) = 1 + 1 * n
 -------------------------------------------------------------------------------
 
 #4 Simplify expression
-
+- T(n) = 2n
 -------------------------------------------------------------------------------
 
 #5 Final result (complexity)
-
+- O(n) Linear curve
 -------------------------------------------------------------------------------
 
 """
